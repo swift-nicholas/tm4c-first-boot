4fb01733a410cabbd86cbeaed1c3c9dae6983eec
Nicholas Jackson <nicholas@swiftperformance.com>
2021-11-19 14:21:19 +1000

    bootloader: flash LED to identify bootloader code.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
 #include <stdint.h>
 #include "memory_map.h"
+#include <stdbool.h>
+
+#include <driverlib/gpio.h>
+#include <driverlib/interrupt.h>
+#include <driverlib/pin_map.h>
+#include <driverlib/sysctl.h>
+#include <inc/hw_memmap.h>
+
+/* System clock for component clocking */
+uint32_t g_ui32SysClock;
 
 __attribute__((naked)) static void start_app(uint32_t pc, uint32_t sp) {
   __asm(
@@ -9,13 +19,48 @@ __attribute__((naked)) static void start_app(uint32_t pc, uint32_t sp) {
           ");
 }
 
+void system_init(void);
+void system_deinit(void);
+
+static void delay() {
+  for (int i = 0; i < 250000; i++) {
+    __asm(" nop");
+  }
+}
+
 int main(void) {
-  uint32_t *app_code = (uint32_t *)&__bootloader_addr__;
+  uint32_t *app_code = (uint32_t *)&__app_start__;
   uint32_t app_sp = app_code[0];
   uint32_t app_reset = app_code[1];
+
+  system_init();
+
+  for (int i = 0; i < 20; i++) {
+    delay();
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0x0);
+    delay();
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
+  }
+
+  system_deinit();
 
   start_app(app_reset, app_sp);
 
   while (1)
     ;
 }
+
+void system_init(void) {
+  /* configure the clock for 120mhz */
+  g_ui32SysClock = SysCtlClockFreqSet(
+      (SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_240), 120000000);
+
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+  while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))
+    ;
+
+  GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+  GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
+}
+
+void system_deinit(void) { SysCtlPeripheralDisable(SYSCTL_PERIPH_GPION); }
